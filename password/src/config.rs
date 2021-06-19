b12c2e23f7c3e99114a4caa97b1a5a0ebf9002fd
daxpedda <daxpedda@gmail.com>
2021-06-20 00:36:13 +0200

    Fix CI

--- password/src/config.rs
+++ password/src/config.rs
@@ -7,8 +7,9 @@ use crate::cipher_suite::CipherSuite;
 /// Common password configuration between server and client.
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Config {
-	/// Inner type implementing [`CipherSuite`](ciphersuite::CipherSuite) to
-	/// prevent exposing it publicly.
+	/// Inner type implementing
+	/// [`CipherSuite`](opaque_ke::ciphersuite::CipherSuite) to prevent exposing
+	/// it publicly.
 	cipher_suite: CipherSuite,
 }
 
