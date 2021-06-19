54fef6bd0e53b90be0cb24949ae66f5a5a3b7c0e
daxpedda <daxpedda@gmail.com>
2021-06-19 22:27:27 +0200

    Add coverage xtask

---
+++ xtask/src/main.rs
@@ -0,0 +1,25 @@
+use khonsu_tools::{
+	anyhow,
+	code_coverage::{self, CodeCoverage},
+};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub enum Commands {
+	GenerateCodeCoverageReport,
+}
+
+fn main() -> anyhow::Result<()> {
+	let command = Commands::from_args();
+	match command {
+		Commands::GenerateCodeCoverageReport => CodeCoverage::<CoverageConfig>::execute(),
+	}
+}
+
+struct CoverageConfig;
+
+impl code_coverage::Config for CoverageConfig {
+	fn ignore_paths() -> Vec<String> {
+		vec![String::from("password/examples/*")]
+	}
+}
